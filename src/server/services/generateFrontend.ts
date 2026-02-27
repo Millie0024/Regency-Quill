f64159605f9cabdcb0566e3c825392fb1f991b4d
Soumili Chakraborty <soumilichakraborty002@gmail.com>
2026-02-28 05:29:51 +0530

    more chsnges

--- src/server/services/generateFrontend.ts
+++ src/server/services/generateFrontend.ts
@@ -1,11 +1,12 @@
 import { GenerationPayload, GenerationResponse } from "../../types/generation";
 
-const API_URL = "http://localhost:5000/api/generate";
+const API_URL = import.meta.env.VITE_API_URL;
+console.log(API_URL);
 
 export async function generateContent(
   payload: GenerationPayload
 ): Promise<GenerationResponse> {
-  const res = await fetch(API_URL, {
+  const res = await fetch(`${API_URL}/api/generate`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload),
